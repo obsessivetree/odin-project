d5467d7d5d5ef313a4172eb527dc6e83d7445209
Daniel Beatty <danielvinbea@gmail.com>
2021-08-09 21:21:30 -0700

    made event listener for user input. made nicer ui. NEED TO MAKE GAME ( ) KEEP GOING AFTER 1 CLICK

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -3,16 +3,11 @@ function game() {
     let userWins = 0;
     let computerWins = 0;
     let roundEnd = false;
-    let userPlay = "";
+    let userPlay;
     let computerPlay = "";
+    let userScore = document.querySelector("#human-score").textContent;
+    let computerScore = document.querySelector("#machine-score").textContent;
     // check for which input
-
-    function userPlayFunc() {
-        userPlay = "";
-        while (userPlay !== "rock" && userPlay !== "scissors" && userPlay !== "paper"){
-            userPlay = prompt("Rock, Paper, or Scissors?: ").toLowerCase();
-        }
-    }
 
     // generate random play from computer
     function computerPlayFunc() {
@@ -20,60 +15,41 @@ function game() {
         const playOptions = ['rock','paper','scissors'];
         computerPlay = playOptions[index];
     }
+    // user input
 
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => button.addEventListener('click', (e) => {
+        playRound(e.target.id)
+        console.log(e.target.id)}))
     
-    function playRound() {
+    function playRound(userPlay) {
         while (roundEnd === false){
     // take INPUT of rock paper or scissors from user
 
-            userPlayFunc();
             computerPlayFunc();
             
-    // const userPlay = userPlayFunc();
     // compare answers
             if (userPlay !== computerPlay) {
                 if (userPlay === 'rock' && computerPlay === 'scissors' || userPlay === 'paper' && computerPlay === 'rock' || userPlay === 'scissors' && computerPlay === 'paper') {
                     userWins++;
+                    userScore = userWins;
                     roundEnd = true;
                     alert(`${userPlay.toUpperCase()} beats ${computerPlay.toUpperCase()}! The HUMAN wins! The score is: \nHUMAN ${userWins} - MACHINES ${computerWins}`);
                 } else {
                     computerWins++;
+                    computerScore = computerWins;
                     roundEnd = true;
                     alert(`${computerPlay.toUpperCase()} beats ${userPlay.toUpperCase()}! The MACHINES win! The score is: \HUMAN ${userWins} - MACHINES ${computerWins}`);
                 }
             } else {
                 roundEnd = true;
                 alert("DRAW!")
-            } 
+            }  
         }
-            if (roundEnd === true) {
-                    let answer = "";
-                    while (answer !== 'yes' && answer !== 'no'){
-                        answer = prompt("Play Again? (yes/no): ");
-                    }
-                    if (answer === 'yes'){
-                        roundEnd = false;
-                        playRound();
-                    } else {
-                        alert(`Thanks for playing! The final score is: \nHUMAN ${userWins} - MACHINES ${computerWins}`)
-                    }
-                }
-        
     }
-    playRound();
-    // if (roundEnd === true) {
-    //     let answer = "";
-    //     while (answer !== 'yes' || answer !== 'no'){
-    //         answer = prompt("Play Again? (yes/no): ");
-    //     }
-    //     if (answer === 'yes'){
-    //         roundEnd = false;
-    //     } else {
-    //         return ("Thanks for playing!")
-    //     }
-    // }
-    
+    // NEED TO FIGURE OUT HOW TO KEEP THE GAME GOING AND WAIT FOR CLICKS
+    // while (computerWins < 2 || userWins < 2){    }
 }
+game()
 
 
-console.log(game());
