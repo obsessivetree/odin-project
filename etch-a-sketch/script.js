56e312eb0106d9a60a35e3fd1f652bf4b17f8cb2
Daniel Beatty <danielvinbea@gmail.com>
2021-08-19 10:29:12 -0700

    random color button now works, and color button cycles through the colors correctly.

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -2,7 +2,7 @@
     const main = document.querySelector("main");
     
     // div.textContent = "hello";
-    function makeGrid(n=50){
+    function makeGrid(n=25){
         if (main.firstChild){
             while (main.firstChild){
                 main.removeChild(main.lastChild)
@@ -12,38 +12,80 @@
         main.style.gridTemplateRows = `repeat(${n}, 1fr)`;
         for (let i = 0; i < n**2; i++){
             const div = document.createElement('div');
-            div.addEventListener('mouseover', (e) => {e.target.classList.add("active")} )
             main.appendChild(div);
         }
     }
-    makeGrid();
     
+    
+    function randomColor() {
+        return (colors[Math.floor(Math.random()*colors.length)])
+    }
+
+    function gridColor(color='black', random=false){
+        let divs = document.querySelectorAll("div");
+        divs.forEach(div => {
+            // div.classList.remove(currentColor);
+            div.addEventListener('mouseover', (e) => {
+                // e.target.classList.remove(currentColor);
+                // console.log(e.target.classList)
+                // console.log(currentColor);
+                if (random === false) {
+                    e.target.className = color
+                } else {
+                    e.target.className = randomColor();
+                }
+            });
+            // console.log(color);
+            currentColor = color;
+            colorBtn.textContent = `Color: ${color}`
+    })};
+
     const colorBtn = document.querySelector("#color-btn");
     const resetBtn = document.querySelector("#reset-btn");
     const randomBtn = document.querySelector("#random-btn");
     const fadeBtn = document.querySelector("#fade-btn");
     
+    const colors = ['black', 'red', 'green', 'blue', 'magenta', 'cyan', 'yellow'];
+
+    var currentColor;
+
+    makeGrid();
+    gridColor();
 
     resetBtn.addEventListener('click', ()=> {
         do{
-            var dimensions = parseInt(prompt("Dimensions (1-100): ", "50"));
+            var dimensions = parseInt(prompt("Dimensions (1-100): ", "25"));
         } while (isNaN(dimensions) || dimensions > 100 || dimensions < 1);
         makeGrid(dimensions)
+        gridColor()
+        randomBtn.textContent = "Random: off"
     })
 
-    const black = 'rgb(0, 0, 0)';
-    const red = 'rgb(255, 0, 0)';
-    const green = 'rgb(0, 255, 0)';
-    const blue = 'rgb(0, 0, 255)';
-    const magenta = 'rgb(255, 0, 255)';
-    const cyan = 'rgb(0, 255, 255)';
-    const yellow = 'rgb(255, 255, 0)';
+    randomBtn.addEventListener('click', () => {
+        if (randomBtn.textContent === "Random: off"){
+            gridColor('black', random=true);
+            randomBtn.textContent = "Random: on"
+        } else {
+            randomBtn.textContent = "Random: off"
+            gridColor(currentColor, random=false)
+        }
+    });
 
-    const colors = [black, red, green, blue, magenta, cyan, yellow];
+    colorBtn.addEventListener('click', () => {
+        randomBtn.textContent = "Random: off"
+        for (let i = 0; i < colors.length; i++){
+            if (colors[i] === currentColor) {
+                if (i+1 < colors.length){
+                    var color = colors[i+1];
+                    break
+                }
+            } else {
+                color = colors[0]
+            }
+        }
 
-
-    // colorBtn.addEventListener('click', function changeColor(e){
-    //     colors.forEach(()=>{
-    //         if ()
-    //     })
-    // })
+        gridColor(color);
+        currentColor = color;
+        // console.log(randColor)
+        
+    });
