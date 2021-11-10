02ccccf08078c226648128779b3b3ed76acf6be2
Daniel Beatty <danielvinbea@gmail.com>
2021-11-10 12:47:38 -0800

    finished all functional components of this exercise

--- library-exercise/script.js
+++ library-exercise/script.js
@@ -1,64 +1,26 @@
-// Document Elements
-//TODO
-    //allow for user to change read status from table view
-    //make input got to table properly
-    //store to local storage so info doesn't disappear
-
-
-//function i found to check if i can store locally
-    // function storageAvailable(type) {
-    //     var storage;
-    //     try {
-    //         storage = window[type];
-    //         var x = '__storage_test__';
-    //         storage.setItem(x, x);
-    //         storage.removeItem(x);
-    //         return true;
-    //     }
-    //     catch(e) {
-    //         return e instanceof DOMException && (
-    //             // everything except Firefox
-    //             e.code === 22 ||
-    //             // Firefox
-    //             e.code === 1014 ||
-    //             // test name field too, because code might not be present
-    //             // everything except Firefox
-    //             e.name === 'QuotaExceededError' ||
-    //             // Firefox
-    //             e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-    //             // acknowledge QuotaExceededError only if there's something already stored
-    //             (storage && storage.length !== 0);
-    //     }
-    // }
-
-    
-    // if (storageAvailable('localStorage')) {
-    //     console.log('yes')
-    //   }
-    //   else {
-    //     console.log('no')
-    //   }
-
 const newBookButton    = document.querySelector("#new-book-button");
 const readButton       = document.querySelector("#read");
 const saveButton       = document.querySelector("#saveNewBook");
 const cancelButton     = document.querySelector("#cancel");
 const form             = document.querySelector("#form");
 const libDiv           = document.querySelector("#lib-div");
-// const cardReadButton   = document.querySelectorAll('.card-read-button');
+
+let isFormVisible = false;
 
 
 function toggleFormDisplay() {
-    if (form.style.visibility === "collapse"){
-        form.style.visibility           = "visible"
-        form.style.position             = "relative"
+    if (isFormVisible){
+        form.style.visibility           = "visible";
+        form.style.position             = "relative";
         newBookButton.style.visibility  = "collapse";
         newBookButton.style.position    = "absolute";
+        isFormVisible                   = false;
     } else {
-        form.style.position             = "absolute"
+        form.style.position             = "absolute";
         form.style.visibility           = "collapse";
         newBookButton.style.visibility  = "visible";
         newBookButton.style.position    = "relative";
+        isFormVisible                   = true;
     }
 }
 
@@ -73,6 +35,7 @@ saveButton.addEventListener('click', function addBookToLibrary() {
     const numOfPages   = document.getElementById("numOfPagesInp").value;
     const isRead       = document.getElementById("isReadInp").checked;
     const newBook      = new Book(title, author, numOfPages, isRead);
+   
     myLibrary.push(newBook);
     
     window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
@@ -92,26 +55,19 @@ function Book(title, author, numOfPages, isRead=false) {
 }
 
 
-let myLibrary = [
-    new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 400, false),
-    new Book("Percy Jackson", "Rick Riordan", 294, true),
-
-];
+let myLibrary = [];
 
 
 function displayBooks(){
     libDiv.innerHTML = '';
     myLibrary = JSON.parse(window.localStorage.getItem('myLibrary'));
-    // console.log(window.localStorage.keys(0))
     myLibrary.forEach(book => {
         const newDiv = document.createElement('div');
         newDiv.className = 'book-card';
         Object.keys(book).forEach(key => {
-            
             const smallDiv = document.createElement('div');
             smallDiv.className = 'small-div';
             if (key === 'isRead'){
-                const isReadLabel = document.createElement('label');
                 const isReadButton = document.createElement('input');
                 isReadButton.type = 'checkbox';
                 smallDiv.textContent += 'Read:'
@@ -119,32 +75,39 @@ function displayBooks(){
                 if (book[key] === true){
                     isReadButton.checked = true;
                 }
+                isReadButton.addEventListener('click', (e) => {
+                    myLibrary.forEach(book => {
+                        if (book.title === e.target.parentNode.parentNode.firstChild.textContent){
+                            book.isRead = e.target.checked;
+                            window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+                        }
+                    })
+                })
             } else {
-                smallDiv.textContent = book[key];
+                let text  = '';
+                let pages = '';
+                key === 'author' ? text = 'by ' : key === "numOfPages" ? pages = ' pages' : pages = '';
+                
+                book[key] !== '' ? smallDiv.textContent = text + book[key] + pages : smallDiv.textContent = '';
             }
             newDiv.appendChild(smallDiv);
         });
         const deleteBtn = document.createElement('button');
         deleteBtn.textContent = 'Delete';
         deleteBtn.className = 'delete-button';
+        deleteBtn.addEventListener('click', (e) => {
+            const bookDiv = e.target.parentNode;
+            const bookTitle = bookDiv.firstChild.textContent;
+            myLibrary = myLibrary.filter(book => book.title !== bookTitle)
+            window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+            bookDiv.remove();
+        });
         newDiv.appendChild(deleteBtn);
         libDiv.appendChild(newDiv);
-        var deleteButtons = document.querySelectorAll('.delete-button');
-        deleteButtons.forEach(button => {
-            button.addEventListener('click', (e) => {
-                e.target.parentElement.remove()
-                window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-            })
-        });
     });
 };
 
 
 if (window.localStorage.getItem('myLibrary') !== null) {
     displayBooks();
-}
-
-// Still need to:
-    // Make delete button remove book from local storage
-    // Make Read checkbox on book card reflect in local storage
-    // prettify
+};
